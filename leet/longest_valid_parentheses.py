48c1b52d185de5c0f4eb7f2b18362eb3358f0a81
trong-nguyen <nguyentrong@gmail.com>
2018-01-02 02:56:27 -0500

    A clever and briliant approach consulted which gives O(n) solution to improvised parentheses problem.

--- leet/longest_valid_parentheses.py
+++ leet/longest_valid_parentheses.py
@@ -12,25 +12,45 @@ Another example is ")()())", where the longest valid parentheses substring is "(
 import sys
 from utils.templates import fail_string
 
-def longest_parentheses(s):
-    def _longest_parentheses(s, opener):
-        max_count = 0
-        count = 0
-        bucket = 0
-        for c in s:
-            if c == opener:
-                bucket += 1
-            elif bucket > 0:
-                bucket -= 1
-                count += 1
-                max_count = max(max_count, count)
-            else:
-                count = 0
-            print bucket, count
-        return max_count * 2
+# def valid_parentheses(s):
 
-    print _longest_parentheses(s, '('), _longest_parentheses(s[::-1], ')')
-    return min(_longest_parentheses(s, '('), _longest_parentheses(s[::-1], ')'))
+def longest_parentheses(s):
+    stack = [-1]
+    max_length = 0
+    for i, c in enumerate(s):
+        if c == '(':
+            stack.append(i)
+        elif not stack:
+            stack.append(i)
+        else:
+            stack.pop()
+            if stack:
+                max_length = max(i-stack[-1], max_length)
+            else:
+                stack.append(i)
+    return max_length
+
+
+
+# def longest_parentheses(s):
+#     def _longest_parentheses(s, opener):
+#         max_count = 0
+#         count = 0
+#         bucket = 0
+#         for i, c in enumerate(s):
+#             if c == opener:
+#                 bucket += 1
+#             elif bucket > 0:
+#                 bucket -= 1
+#                 count += 1
+#                 max_count = max(max_count, count)
+#             else:
+#                 count = 0
+#             print bucket, count
+#         return max_count * 2
+
+#     print _longest_parentheses(s, '('), _longest_parentheses(s[::-1], ')')
+#     return min(_longest_parentheses(s, '('), _longest_parentheses(s[::-1], ')'))
 
 
 class Solution(object):
@@ -45,11 +65,12 @@ class Solution(object):
 
 def test():
     for case, ans in [
-        (['(()'], 1),
+        (['(()(()(()'], 2),
+        (['(()'], 2),
         (["))))())()()(()"], 4),
-        # (["()(()"], 2),
-        # (["(()"], 2),
-        # ([')()())'], 4),
+        (["()(()"], 2),
+        (["(()"], 2),
+        ([')()())'], 4),
     ]:
         res = longest_parentheses(*case)
         try:
