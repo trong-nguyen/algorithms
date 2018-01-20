9edcdc000b2f0e236ea7805d94eebd9b1fa68325
trong-nguyen <nguyentrong@gmail.com>
2018-01-19 23:42:53 -0500

    Building failure functions, upper bounds appear to be correct, but can still be optimized based on the character at current index i

--- leet/shortest_palindrome.py
+++ leet/shortest_palindrome.py
@@ -11,11 +11,62 @@ Given "aacecaaa", return "aacecaaa".
 Given "abcd", return "dcbabcd".
 """
 
+def build_failure_table(w):
+    if not w:
+        return []
+
+    table = [-1] + [0] * (len(w) - 1)
+    for i in range(2, len(w)):
+        # if table[i-1] == 0:
+        #     if w[i] == w[0]:
+        #         table[i] = -1
+        #     elif w[i-1] == w[0]:
+        #         table[i] = 1
+        # else:
+        #     d = i - 1 - table[i-1]
+        #     if w[i-1] == w[i-d-1]:
+        #         table[i] = table[i-1] + 1
+
+        d = i - 1 - table[i-1]
+        if w[i-1] == w[i-d-1]:
+            table[i] = table[i-1] + 1
+
+    # now checking w[i] which have boolean values (i.e. t[i]
+    # corresponding to s[i] being not w[i])
+    for i in range(2, len(w)):
+        while w[i] == w[table[i]] and table[i] >= 0:
+            table[i] -= 1
+
+    return table
+
 def knuth_morris_pratt(s):
     return 's'
 
 import sys
 from utils.templates import fail_string
+
+def unit_test():
+    def aligned_str(array, room):
+        return ' '.join(map('{{:>{}}}'.format(room).format, array))
+
+    spaces = 3
+    w = 'ABCDABD'
+    t = build_failure_table(w)
+    print aligned_str(w, 3)
+    print aligned_str(t, 3)
+    print '\n'
+
+    w = 'ABACABABC'
+    t = build_failure_table(w)
+    print aligned_str(w, 3)
+    print aligned_str(t, 3)
+    print '\n'
+
+    w = 'PARTICIPATE IN PARACHUTE'
+    t = build_failure_table(w)
+    print aligned_str(w, 3)
+    print aligned_str(t, 3)
+    print '\n'
 
 def test():
     for case, ans in [
@@ -30,4 +81,5 @@ def test():
             sys.exit(status)
 
 if __name__ == '__main__':
+    unit_test()
     test()
