89763c1d8f3965c8b7839021506d779dc9c76f95
trong-nguyen <nguyentrong@gmail.com>
2018-01-20 20:21:15 -0500

    O(n) time for shortest palindrome achieved. Amazing amount of time spent to understand KMP, aka Knuth-Morris-Pratt algo. Amazing.

--- leet/shortest_palindrome.py
+++ leet/shortest_palindrome.py
@@ -11,31 +11,104 @@ Given "aacecaaa", return "aacecaaa".
 Given "abcd", return "dcbabcd".
 """
 
+def slide_build_table(w):
+    if not w:
+        return []
+
+    table = [-1] + [0] * (len(w) - 1)
+
+    j = 0
+    for i in range(1, len(w)):
+        if w[i] == w[j]:
+            table[i] = table[j]
+            j += 1
+        else:
+            table[i] = j
+            # reset j jumping until find the closest w[i] prior to j
+            while True:
+                j = table[j]
+                if w[i] == w[j] or j < 0:
+                    break
+            j += 1
+    return table
+
+def search(s, w, table):
+    i = 0
+    j = 0
+
+    for i in range(len(s)):
+        if s[i] == w[j]:
+            j += 1
+        else:
+            j = table[j]
+            j += 1
+
+    return s + w[j:]
+
+def build_shortest_palindrome(w):
+    t = slide_build_table(w)
+    return search(w[::-1], w, t)
+
+class Solution(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return build_shortest_palindrome(s)
+
 def build_failure_table(w):
     if not w:
         return []
 
     table = [-1] + [0] * (len(w) - 1)
-    for i in range(2, len(w)):
-        # if table[i-1] == 0:
-        #     if w[i] == w[0]:
-        #         table[i] = -1
-        #     elif w[i-1] == w[0]:
-        #         table[i] = 1
-        # else:
-        #     d = i - 1 - table[i-1]
-        #     if w[i-1] == w[i-d-1]:
-        #         table[i] = table[i-1] + 1
+    for i in range(1, len(w)):
 
-        d = i - 1 - table[i-1]
-        if w[i-1] == w[i-d-1]:
+        d = table[i-1]
+        if w[i-1] == w[d]:
             table[i] = table[i-1] + 1
+
+
+    return table
+
+def second_sweep(w, table):
+    def satisfied(i, d, w):
+        return w[i] != w[d] and w[i-d:i] == w[:d]
 
     # now checking w[i] which have boolean values (i.e. t[i]
     # corresponding to s[i] being not w[i])
-    for i in range(2, len(w)):
-        while w[i] == w[table[i]] and table[i] >= 0:
-            table[i] -= 1
+    for i in range(1, len(w)):
+        d = table[i]
+
+        if w[i] == w[d]:
+            if d == 0:
+                table[i] = -1
+            else:
+                for j in range(d-1, -1, -1):
+                    if satisfied(i, j, w):
+                        table[i] = j
+                        break
+                    elif j == 0: # if not satisfied even when j=0
+                        table[i] = -1
+
+
+    return table
+
+def third_sweep(w, table):
+    def satisfied(i, d, w):
+        return w[i] != w[d] and w[i-d:i] == w[:d]
+
+    # now checking w[i] which have boolean values (i.e. t[i]
+    # corresponding to s[i] being not w[i])
+    for i in range(1, len(w)):
+        d = table[i-1]
+
+        if d > table[i]:
+            for j in range(d-1, -1, -1):
+                if satisfied(i, j, w):
+                    table[i] = j
+                    break
+
 
     return table
 
@@ -46,34 +119,76 @@ import sys
 from utils.templates import fail_string
 
 def unit_test():
-    def aligned_str(array, room):
+    def aligned_str(array, room=3):
         return ' '.join(map('{{:>{}}}'.format(room).format, array))
 
-    spaces = 3
     w = 'ABCDABD'
-    t = build_failure_table(w)
-    print aligned_str(w, 3)
-    print aligned_str(t, 3)
-    print '\n'
+    t = slide_build_table(w)
+    assert t == [-1, 0, 0, 0, -1, 0, 2]
+    # t = build_failure_table(w)
+    # print aligned_str(w)
+    # print aligned_str(t)
+    # t = third_sweep(w, t)
+    # print aligned_str(t)
+    # print aligned_str(second_sweep(w, t))
+    # print aligned_str(slide_build_table(w))-1 0   0   0   0   0   0   -1  0   2   0   0   0   0   0   -1  0   0   3   0   0   0   0   0
 
     w = 'ABACABABC'
-    t = build_failure_table(w)
-    print aligned_str(w, 3)
-    print aligned_str(t, 3)
-    print '\n'
+    t = slide_build_table(w)
+    assert t == [-1, 0, -1, 1, -1, 0, -1, 3, 2]
+    # t = build_failure_table(w)
+    # print aligned_str(w)
+    # print aligned_str(t)
+    # t = third_sweep(w, t)
+    # print aligned_str(t)
+    # print aligned_str(second_sweep(w, t))
+    # print aligned_str(slide_build_table(w))-1 0   0   0   0   0   0   -1  0   2   0   0   0   0   0   -1  0   0   3   0   0   0   0   0
 
     w = 'PARTICIPATE IN PARACHUTE'
-    t = build_failure_table(w)
-    print aligned_str(w, 3)
-    print aligned_str(t, 3)
+    t = slide_build_table(w)
+    assert t == [-1, 0, 0, 0, 0, 0, 0, -1, 0, 2, 0, 0, 0, 0, 0, -1, 0, 0, 3, 0, 0, 0, 0, 0]
+    # t = build_failure_table(w)
+    # print aligned_str(w)
+    # print aligned_str(t)
+    # t = third_sweep(w, t)
+    # print aligned_str(t)
+    # print aligned_str(second_sweep(w, t))
+    # print aligned_str(slide_build_table(w))-1 0   0   0   0   0   0   -1  0   2   0   0   0   0   0   -1  0   0   3   0   0   0   0   0
+
+    n = 1000000
+    w = 'X' * n
+    t = slide_build_table(w)
+    assert t == [-1] * n
+    # t = build_failure_table(w)
+    # print aligned_str(w)
+    # print aligned_str(t)
+    # t = third_sweep(w, t)
+    # print aligned_str(t)
+    # print aligned_str(second_sweep(w, t))
+    # print aligned_str(slide_build_table(w))-1 0   0   0   0   0   0   -1  0   2   0   0   0   0   0   -1  0   0   3   0   0   0   0   0
+
+
+    print aligned_str('aaacecaa')
+    print aligned_str(slide_build_table('aaacecaa'))
     print '\n'
 
+    w = 'AABBA'
+    s = w[::-1]
+    t = slide_build_table(w)
+    print aligned_str(search(s, w, t))
 def test():
+    solution = Solution()
     for case, ans in [
-        (['aacecaaa'], 'aacecaaa'),
+        (["aabba"], "abbaabba"),
+        (['aea'], 'aea'),
+        (['a'], 'a'),
+        ([''], ''),
+        (['aacecaaa'], 'aaacecaaa'),
         (['abcd'], 'dcbabcd'),
+        (['xxx'*1000 + 'y' + 'xxx'*1000], 'xxx'*1000 + 'y' + 'xxx'*1000),
+        (['xxx'*1000], 'xxx'*1000),
     ]:
-        res = knuth_morris_pratt(*case)
+        res = solution.shortestPalindrome(*case)
         try:
             assert res == ans
         except AssertionError as e:
