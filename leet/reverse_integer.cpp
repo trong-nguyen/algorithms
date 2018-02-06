f3dd1051fcb5227f10b69ece2cae4d984245246e
trong-nguyen <nguyentrong@gmail.com>
2018-02-06 01:05:19 -0500

    Simple but usefull C++ practice. Reverse a number.

--- leet/reverse_integer.cpp
+++ leet/reverse_integer.cpp
@@ -1,32 +1,46 @@
+/*
+
+Reverse an integer, handling overflow cases
+
+-2^31 + 1 <= reverse(x) <= 2^31
+
+get the digits of number and convert them from highest order to lowest
+*/
+
 #include <vector>
 #include <iostream>
 #include <cassert>
 #include <cmath>
 
-#define MAX_POSITIVE 1073741824
-#define MAX_NEGATIVE -1073741823
+#define MAX_POSITIVE 2147483648
+#define MAX_NEGATIVE -2147483647
 
-int numDigits(int x) {
-    int d = 10; // 32 bit - 1(signed)
-    while(x < pow(10, --d));
-    return d;
+int numDigits(long x) {
+    // x is positive
+    assert(x >= 0);
+
+    int d = 0; // 32 bit - 1(signed)
+    while(pow(10, ++d) <= x);
+    return d - 1;
 }
 
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse(long x) {
+        const int sign = x >= 0 ? 1 : -1;
+        x = x * sign; // make it positive
         const int d = numDigits(x);
 
-        int reversed = 0;
+        long reversed = 0;
 
         for(int i = d; i >=0; i--) {
-            const auto units = pow(10, i);
+            const long units = pow(10, i);
             int xi = x / units; // note the implitcit conversion
-            reversed += x / units * pow(10, d - i);
-            x -= units;
-            std::cout << x << " " << reversed << "\n";
+            reversed += xi * pow(10, d - i);
+            x = x % units;
         }
 
+        reversed *= sign;
 
         if(reversed > MAX_POSITIVE || reversed < MAX_NEGATIVE) {
             //overflow
@@ -43,12 +57,32 @@ int main() {
 
     // typedef std::vector<int> Array;
 
-    std::vector<int> cases = {
-        123
+    std::vector<long> cases = {
+        10,
+        123,
+        120,
+        (long)(pow(2, 30)) + 1,
+        4281473701,
+        -(long)(pow(2, 30)),
+        -4281473701,
+        -3281473701,
+        12000000,
+        120000021,
+        -2147483412
     };
 
-    std::vector<int> ans = {
-        321
+    std::vector<long> ans = {
+        1,
+        321,
+        21,
+        0,
+        1073741824,
+        0,
+        -1073741824,
+        -1073741823,
+        21,
+        120000021,
+        -2143847412
     };
 
     assert(cases.size() == ans.size() && "Cases and ans sets size should be matched!");
@@ -60,6 +94,8 @@ int main() {
             std::cout << cases[i];
             std::cout << "] failed, expected " << ans[i] << ", got " <<  res << "\n";
             throw "Test failed\n";
+        } else {
+            std::cout << "case " << cases[i] << ", res " << res << " passed\n";
         }
     }
 }
