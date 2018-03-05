056b1dfaca03bb15db98ba733ae9d8b45aced3c7
trong-nguyen <nguyentrong@gmail.com>
2018-03-04 20:11:25 -0500

    As expected, BFS worked

--- leet/max_width_of_bin_tree.cpp
+++ leet/max_width_of_bin_tree.cpp
@@ -17,130 +17,133 @@ struct TreeNode {
 #include <list>
 class Solution {
 public:
-    typedef std::list<TreeNode *> Path;
-
-    bool hasChildren(const TreeNode * node) {
-        return node->left != NULL || node->right != NULL;
-    }
-
-    bool hasExactlyOneChild(const TreeNode * node) {
-        if(node->left) {
-            return node->right == NULL;
-        } else if(node->right) {
-            return node->left == NULL;
-        } else {
-            return false;
-        }
-    }
-
-    bool hasBothChildren(const TreeNode * node) {
-        return node->left != NULL && node->right != NULL;
-    }
-
-    bool bothNotStuck(TreeNode * leftPointer, TreeNode * rightPointer) {
-        return hasChildren(leftPointer) && hasChildren(rightPointer);
-    }
-
-    bool oneStuck(TreeNode * leftPointer, TreeNode * rightPointer) {
-        return hasChildren(leftPointer) || hasChildren(rightPointer);
-    }
-
-    void fastforwardUntillDiverged(Path & path) {
-        TreeNode * node=0;
-        // pop the path untill diverged
-        while(!path.empty()) {
-            node = path.pop_front();
-            if(hasBothChildren(node)) {
-                break;
-            }
-        }
-
-        // if path empty and there is till children
-        while(node && hasExactlyOneChild(node)) {
-            node = (node->left ? node->left : node->right);
-        }
-
-        // node could be NULL or node must have 2 children
-        // path now if not empty starts after node
-        return node;
-    }
+    typedef std::list<TreeNode *> NodeList;
 
     int widthOfBinaryTree(TreeNode* root) {
+        // let's reuse the value since it has no role in this algorithm
         if(!root) {
             return 0;
         }
 
-        TreeNode * leftPointer = root;
-        TreeNode * rightPointer = root;
-
-        Path leftPath, rightPath;
-
+        NodeList currentLevelNodes;
+        root->val = 0;
+        currentLevelNodes.push_back(root);
         int maxWidth = 1;
-        while(bothNotStuck(leftPointer, rightPointer)) {
-            if(oneStuck(leftPointer, rightPointer)) {
-                maxWidth = std::max(maxWidth, computeWidth(leftPath, rightPath));
-
-                DivergedPoint * new_root = NULL;
-                if(hasChildren(leftPointer)) {
-                    rightPath.clear();
-                    new_root = fastforwardUntillDiverged(leftPath);
-                    if(!new_root) {
-                        return maxWidth;
-                    }
-                    rightPath.push_back(new_root->right);
-
-                } else {
-                    leftPath.clear();
-                    new_root = fastforwardUntillDiverged(rightPath);
-                    if(!new_root) {
-                        return maxWidth;
-                    }
-                    leftPath.push_back(new_root->left);
+        while(!currentLevelNodes.empty()) {
+            NodeList nextLevelNodes;
+            for(auto node: currentLevelNodes) {
+                if(node->left) {
+                    nextLevelNodes.push_back(node->left);
+                    node->left->val = node->val * 2;
+                }
+                if(node->right) {
+                    nextLevelNodes.push_back(node->right);
+                    node->right->val = node->val * 2 + 1;
                 }
             }
 
-            // now both left and right must be advanceble
-            advance(leftPath, "left");
-            advance(rightPath, "right");
+            if(!nextLevelNodes.empty()) {
+                maxWidth = std::max(maxWidth, nextLevelNodes.back()->val - nextLevelNodes.front()->val + 1);
+            }
+
+            currentLevelNodes = nextLevelNodes;
         }
+
+        return maxWidth;
     }
 };
 
+#include <memory>
 #include "gtest/gtest.h"
 
 namespace {
-    typedef std::vector<int> Array;
-    struct AnswerSet {
-        Array problem;
-        bool answer;
+    typedef TreeNode * Problem;
+    typedef int Answer;
 
-        AnswerSet(const Array & p, bool a) {
+    class AnswerSet {
+    public:
+        Problem problem;
+        Answer answer;
+
+        AnswerSet(Problem & p, const Answer & a) {
             problem = p;
             answer = a;
         }
     };
 
-    class PredictWinnerTest: public ::testing::Test {
+    class WidthOfBinaryTreeTest: public ::testing::Test {
     protected:
         Solution solution;
+        std::vector<TreeNode> nodeVault;
+        int nodesUsed;
 
         virtual void SetUp() {
             solution = Solution();
+            nodeVault.resize(1000, TreeNode(0));
+            nodesUsed = 0;
         }
 
-        virtual ~PredictWinnerTest() {}
+        TreeNode * getOneNode() {
+            if(nodesUsed < nodeVault.size()) {
+                return &nodeVault[nodesUsed++];
+            }
+
+            throw "All nodes used up. Increase nodeVault size.\n";
+        }
+
+        virtual ~WidthOfBinaryTreeTest() {}
     };
 
-    TEST_F(PredictWinnerTest, TestPredictions) {
-        for(const auto & as: std::vector<AnswerSet> ({
-            AnswerSet(Array({0}), true)
-            , AnswerSet(Array({1, 3, 1}), false)
-            , AnswerSet(Array({5, 233, 7}), false)
-            , AnswerSet(Array({1, 5, 233, 7}), true)
-            , AnswerSet(Array({2, 4, 55, 6, 8}), false)
-        })) {
-            ASSERT_TRUE(solution.PredictTheWinner(as.problem) == as.answer);
-        }
+    TEST_F(WidthOfBinaryTreeTest, TestWidth1) {
+        TreeNode * root = getOneNode();
+        root->left = getOneNode();
+        root->right = getOneNode();
+        root->left->left = getOneNode();
+        root->left->right = getOneNode();
+        root->right->right = getOneNode();
+
+        AnswerSet as(root, 4);
+
+        ASSERT_EQ(solution.widthOfBinaryTree(as.problem), as.answer);
+    }
+
+    TEST_F(WidthOfBinaryTreeTest, TestWidth2) {
+        TreeNode * root = getOneNode();
+        root->left = getOneNode();
+        root->left->left = getOneNode();
+        root->left->right = getOneNode();
+
+        AnswerSet as(root, 2);
+
+        ASSERT_EQ(solution.widthOfBinaryTree(as.problem), as.answer);
+    }
+
+
+    TEST_F(WidthOfBinaryTreeTest, TestWidth3) {
+        TreeNode * root = getOneNode();
+        root->left = getOneNode();
+        root->right = getOneNode();
+        root->left->left = getOneNode();
+
+        AnswerSet as(root, 2);
+
+        ASSERT_EQ(solution.widthOfBinaryTree(as.problem), as.answer);
+    }
+
+
+    TEST_F(WidthOfBinaryTreeTest, TestWidth4) {
+        TreeNode * root = getOneNode();
+        root->left = getOneNode();
+        root->left->left = getOneNode();
+        root->left->left->left = getOneNode();
+
+        root->right = getOneNode();
+        root->right->right = getOneNode();
+        root->right->right->right = getOneNode();
+
+        AnswerSet as(root, 8);
+
+        ASSERT_EQ(solution.widthOfBinaryTree(as.problem), as.answer);
     }
 }
 
@@ -149,4 +152,4 @@ int main(int argc, char** argv) {
     return RUN_ALL_TESTS();
 }
 
-// g++ -std=c++11 -Iutils/include/ -Lutils/lib/ virtual_contest_492.cpp -lgtest && ./a.out
+// g++ -std=c++11 -Iutils/include/ -Lutils/lib/ max_width_of_bin_tree.cpp -lgtest && ./a.out
